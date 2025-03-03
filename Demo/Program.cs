93fbb2bdf5df06749acb9748f62c56cb163d0b12
malakelsayyad <malakelsayyad@yahoo.com>
2025-03-03 22:09:00 +0200

    Demo

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -8,7 +8,7 @@ namespace Demo
             builder.Services.AddControllersWithViews();//register built-in mvc services
           
 
-            var app = builder.Build();
+            var app = builder.Build(); 
 
             //app.MapGet("/", () => "Hello World!");
             //app.MapGet("/login", () => "You are signed in!");
